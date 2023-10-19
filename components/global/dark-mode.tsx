e2b097d83dfd30283a05963b4dc2f0b0ebd21a35
kKao2 <thanh291100@gmail.com>
2023-10-19 21:16:05 +0700

    Ok

--- components/global/dark-mode.tsx
+++ components/global/dark-mode.tsx
@@ -2,10 +2,19 @@ import { useSelector } from "react-redux"
 import { selectDarkMode } from "@/features/GlobalSlice"
 import { useEffect } from "react"
 import Scrollbar from 'smooth-scrollbar';
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 
 export default function DarkMode({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    Scrollbar.initAll()
+    Scrollbar.use(OverscrollPlugin);
+    Scrollbar.initAll({
+      damping: 0.14,
+      plugins: {
+        overscroll: {
+          effect: "bounce",
+        },
+      }
+    })
   })
   const darkMode = useSelector(selectDarkMode)
   return (
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ["smooth-scrollbar/plugins/overscroll"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
