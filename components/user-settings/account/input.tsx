65ddf2725eb58fa0dfca3817322aee496001367c
kKao2 <thanh291100@gmail.com>
2023-11-30 12:48:38 +0700

    beta

--- components/user-settings/account/input.tsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ children, label }: { children: React.ReactNode, label: string }) {
-  return (
-    <div className="flex flex-col flex-wrap items-center sm:flex-row gap-y-2">
-      <div className="w-full text-sm font-bold text-gray-200 dark:text-neutral-400 basis-1/3">{label}</div>
-      {children}
-    </div>
-  )
-}
