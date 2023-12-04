a741cac810ad2c9dcd0270a36418e11a3e3f613a
kKao2 <thanh291100@gmail.com>
2023-12-04 18:26:26 +0700

    beta

--- components/global/buttonScrollToTop/buttonScrollToTop.tsx
+++ components/global/buttonScrollToTop/buttonScrollToTop.tsx
@@ -1,15 +1,50 @@
 import { useDetectDirectionScrollY } from "@/hooks/useDetectDirectionScrollY";
 import { useRouter } from "next/router";
+import { CSSTransition } from 'react-transition-group';
+import { Variants, motion, AnimatePresence } from "framer-motion";
 
 export default function ButtonScrollToTop() {
   const router = useRouter()
   const directionScroll = useDetectDirectionScrollY()
+  const variants: Variants = {
+    hidden: {
+      opacity: 0,
+      transform: "scale(0.6)",
+    },
+    show: {
+      opacity: 1,
+      transform: "scale(1)"
+    }
+  }
   return (
-    <button
-      className={`fixed ${router.query.chapterNum ? "bottom-18" : "bottom-2"} right-2 bg-second-green h-8 w-8 md:w-10 md:h-10 rounded-full flex justify-center items-center shadow hover:bg-black transition-all ease-out z-40 ${directionScroll === "up" ? "scale-100" : "scale-0"}`}
-      onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
-    >
-      <svg className="h-3 md:h-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
-    </button>
+    // <CSSTransition
+    //   in={directionScroll === "up"}
+    //   timeout={150}
+    //   unmountOnExit
+    //   classNames="scrollToTopBtn"
+    // >
+    //   <button
+    //     className={`fixed ${router.query.chapterNum ? "bottom-18" : "bottom-2"} right-2 bg-second-green h-8 w-8 md:w-10 md:h-10 rounded-full flex justify-center items-center shadow hover:bg-black`}
+    //     onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+    //   >
+    //     <svg className="h-3 md:h-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
+    //   </button>
+    // </CSSTransition>
+    <AnimatePresence>
+      {directionScroll === "up" && (
+        <motion.button
+          key="scrollToTopBtn"
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          variants={variants}
+          transition={{ duration: 0.15, type: "tween" }}
+          className={`fixed ${router.query.chapterNum ? "bottom-18" : "bottom-2"} right-2 bg-second-green h-8 w-8 md:w-10 md:h-10 rounded-full flex justify-center items-center shadow hover:bg-black transition-colors`}
+          onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+        >
+          <svg className="h-3 md:h-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" /></svg>
+        </motion.button>
+      )}
+    </AnimatePresence>
   )
 }
--- components/global/menu/Menu.tsx
+++ components/global/menu/Menu.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { setSearchName } from "@/features/search/SearchSlice";
 import dynamic from "next/dynamic";
 import { useDarkMode } from 'usehooks-ts'
-import { motion } from "framer-motion"
+import { Variants, motion } from "framer-motion"
 const DynamicSearchBox = dynamic(() => import("../searchBox/SearchBox"))
 const DynamicResponsiveMenu = dynamic(() => import("./ResponsiveMenu"))
 
@@ -20,6 +20,20 @@ export default function Menu() {
   const [zIndex, setZIndex] = useState<string>("-z-20")
   const changeOrder = (value: Order) => {
     dispatch(setSort(value))
+  }
+  const svgVariants: Variants = {
+    hiddenUp: {
+      y: -40,
+      opacity: 0
+    },
+    show: {
+      y: 0,
+      opacity: 1
+    },
+    hiddenDown: {
+      y: 40,
+      opacity: 0
+    }
   }
   return (
     <>
@@ -108,12 +122,9 @@ export default function Menu() {
                 transition={{ type: "tween", duration: 0.4 }}
                 viewport={{ once: true }}
               >
-                <button className="flex items-center justify-center w-8 h-8 transition-colors bg-white rounded-full group hover:bg-black" onClick={() => toggle()}>
-                  {isDarkMode ? (
-                    <svg className="h-5 transition-colors fill-main-green group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" /></svg>
-                  ) : (
-                    <svg className="h-5 transition-colors fill-main-green group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z" /></svg>
-                  )}
+                <button className="flex items-center justify-center w-8 h-8 transition-colors bg-white rounded-full group hover:bg-black overflow-hidden relative" onClick={() => toggle()}>
+                  <motion.svg initial={isDarkMode ? "show" : "hiddenDown"} animate={isDarkMode ? "show" : "hiddenDown"} variants={svgVariants} transition={{ type: "tween", duration: 0.3 }} className={`absolute h-5 transition-colors fill-main-green group-hover:fill-white`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" /></motion.svg>
+                  <motion.svg initial={isDarkMode ? "hiddenUp" : "show"} animate={isDarkMode ? "hiddenUp" : "show"} variants={svgVariants} transition={{ type: "tween", duration: 0.3 }} className={`absolute h-5 transition-colors fill-main-green group-hover:fill-white`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z" /></motion.svg>
                 </button>
               </motion.div>
             </div>
